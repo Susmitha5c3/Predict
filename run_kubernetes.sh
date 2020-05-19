6f180efe297c26af2d73e0ba6f6242f771740ae7
Susmitha5c3 <lakshmisusmitha13@gmail.com>
2020-05-19 22:44:40 +0530

    kubernetes script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=susmithasusmi13/udacityproj
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacityproject --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacityproject 8000:80
